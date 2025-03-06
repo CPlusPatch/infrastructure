09f5bc3379120b0bb01d853df0bee0fe8c9b61ec
Jesse Wierzbinski <contact@cpluspatch.com>
2025-03-06 22:36:52 +0100

    fix: :bug: Use proper images and locations, change names to test stuff

--- servers.tf
+++ servers.tf
@@ -1,7 +1,7 @@
-resource "hcloud_server" "chell" {
-  name                     = "chell"
-  image                    = "arch-linux"
-  server_type              = "ax41-nvme"
+resource "hcloud_server" "test1" {
+  name                     = "test1"
+  image                    = "ubuntu-24.04"
+  server_type              = "cx22"
   location                 = "fsn1"
   ssh_keys                 = [hcloud_ssh_key.jesse.id]
   delete_protection        = true
@@ -24,7 +24,7 @@ resource "hcloud_primary_ip" "fsn1_ipv4_1" {
   name          = "fsn1_ipv4_1"
   type          = "ipv4"
   assignee_type = "server"
-  datacenter    = "fsn1"
+  datacenter    = "fsn1-dc14"
   auto_delete   = false
 }
 
@@ -32,20 +32,20 @@ resource "hcloud_primary_ip" "fsn1_ipv6_1" {
   name          = "fsn1_ipv6_1"
   type          = "ipv6"
   assignee_type = "server"
-  datacenter    = "fsn1"
+  datacenter    = "fsn1-dc14"
   auto_delete   = false
 }
 
 locals {
   servers = [{
-    server = hcloud_server.chell
+    server = hcloud_server.test1
     ipv4   = hcloud_primary_ip.fsn1_ipv4_1
     ipv6   = hcloud_primary_ip.fsn1_ipv6_1
   }]
 
   # DNS configuration to be applied to Cloudflare
   domains = {
-    "uptime.cpluspatch.com" = hcloud_server.chell
-    "stats.cpluspatch.com"  = hcloud_server.chell
+    "test01.cpluspatch.com" = hcloud_server.test1
+    "test02.cpluspatch.com" = hcloud_server.test1
   }
 }
