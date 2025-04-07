f6a2d73368b50a571f5f40c914aa6b0652ac8d79
Jesse Wierzbinski <contact@cpluspatch.com>
2025-04-07 19:10:47 +0200

    fix: :bug: Fix Minecraft MOTD characters being incorrectly escaped

--- nix/services/minecraft.nix
+++ nix/services/minecraft.nix
@@ -44,7 +44,7 @@ in {
         enforce-secure-profile = true;
         enforce-whitelist = true;
         max-players = 100;
-        motd = "\u00a7l \u00a7c          \u00a7k$$\u00a76 Join now for \u00a74\u00a7lFREE ROBUX!\u00a7c  \u00a7k$$\u00a7r\n\u00a7l  \u00a7c               \u00a7k$$\u00a76 Obama is here too! \u00a7c\u00a7k$$";
+        motd = "\\u00a7l \\u00a7c          \\u00a7k$$\\u00a76 Join now for \\u00a74\\u00a7lFREE ROBUX!\\u00a7c  \\u00a7k$$\\u00a7r\\n\\u00a7l  \\u00a7c               \\u00a7k$$\\u00a76 Obama is here too! \\u00a7c\\u00a7k$$";
         online-mode = true;
         pvp = true;
         spawn-protection = 0;
